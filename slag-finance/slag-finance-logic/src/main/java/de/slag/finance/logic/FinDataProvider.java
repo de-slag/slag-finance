7b8b0364835d13b249f9747c3401227731dc01b6
sebastian <sebastian@kallisto.fritz.box>
2019-04-20 11:44:32 +0200

    init finance app

---
+++ slag-finance/slag-finance-logic/src/main/java/de/slag/finance/logic/FinDataProvider.java
@@ -0,0 +1,9 @@
+package de.slag.finance.logic;
+
+import java.math.BigDecimal;
+
+import de.slag.common.base.DataProvider;
+
+public interface FinDataProvider extends DataProvider<BigDecimal> {
+
+}
