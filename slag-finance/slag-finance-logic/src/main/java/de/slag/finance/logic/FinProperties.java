72a457b96205b705eb5d04f834a6b5677ca9b66a
sebastian <sebastian@enceladus>
2019-04-22 09:33:30 +0200

    + static access

--- slag-finance/slag-finance-logic/src/main/java/de/slag/finance/logic/FinProperties.java
+++ slag-finance/slag-finance-logic/src/main/java/de/slag/finance/logic/FinProperties.java
@@ -5,25 +5,38 @@ import java.util.Properties;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 
+import de.slag.common.utils.SlagConfigSupportUtils;
 import de.slag.finance.Constants;
 
 public class FinProperties extends Properties {
-	
+
 	private static final Log LOG = LogFactory.getLog(FinProperties.class);
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public FinProperties(Properties p) {
 		super(p);
 		LOG.debug(p);
 	}
 
-	private static final long serialVersionUID = 1L;
-	
 	public String getFinanceWorkdir() {
 		return getProperty(Constants.FINANCE_WORKDIR);
 	}
-	
+
 	public String getRawDataFile() {
 		return getProperty(Constants.RAW_DATA_FILE);
+	}
+
+	private static FinProperties instance() {
+		return new FinProperties(SlagConfigSupportUtils.getConfigProperties());
+	}
+
+	public static String financeWorkdir() {
+		return instance().getFinanceWorkdir();
+	}
+
+	public static String rawDataFile() {
+		return instance().getRawDataFile();
 	}
 
 }
