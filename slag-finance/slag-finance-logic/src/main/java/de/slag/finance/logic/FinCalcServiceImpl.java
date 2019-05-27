63171dbb544819b9d074e944f2ef0152c6aec920
sebastian <sebastian@kallisto.fritz.box>
2019-05-27 20:00:47 +0200

    merge

--- slag-finance/slag-finance-logic/src/main/java/de/slag/finance/logic/FinCalcServiceImpl.java
+++ slag-finance/slag-finance-logic/src/main/java/de/slag/finance/logic/FinCalcServiceImpl.java
@@ -1,9 +1,15 @@
 package de.slag.finance.logic;
 
+import org.springframework.stereotype.Service;
+
 import de.slag.common.base.BaseException;
 import de.slag.finance.data.model.Kpi;
+import de.slag.finance.logic.service.FinDataPointService;
 
+@Service
 public class FinCalcServiceImpl implements FinCalcService {
+	
+	private FinDataPointService finDataPointService;
 	
 	@Override
 	public FinCalcOutput calc(final FinCalcInput input) {
