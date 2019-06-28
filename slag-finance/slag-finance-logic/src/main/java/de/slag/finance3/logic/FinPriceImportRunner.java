67a14829280a4f2fce27dc717917fda74e4f6c37
slipp <slipp@NB308.coinor.local>
2019-06-28 10:56:35 +0200

    monitoring improved

--- slag-finance/slag-finance-logic/src/main/java/de/slag/finance3/logic/FinPriceImportRunner.java
+++ slag-finance/slag-finance-logic/src/main/java/de/slag/finance3/logic/FinPriceImportRunner.java
@@ -3,18 +3,23 @@ package de.slag.finance3.logic;
 import java.math.BigDecimal;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.Collection;
 import java.util.Date;
 import java.util.Objects;
 import java.util.Properties;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 
 import de.slag.common.XiDataDao;
 import de.slag.common.base.BaseException;
 import de.slag.common.model.XiData;
 import de.slag.finance.FinPriceDao;
-import de.slag.finance.data.FinDataPointDao;
 import de.slag.finance.model.FinPrice;
 
 public class FinPriceImportRunner implements Runnable {
+
+	private static final Log LOG = LogFactory.getLog(FinPriceImportRunner.class);
 
 	private static final String DD_MM_YYYY = "dd.MM.yyyy";
 
@@ -22,7 +27,13 @@ public class FinPriceImportRunner implements Runnable {
 
 	private final XiDataDao xiDataDao;
 
-	public FinPriceImportRunner( FinPriceDao finPriceDao, XiDataDao xiDataDao) {
+	private int imported = 0;
+
+	private long start;
+
+	private long end;
+
+	public FinPriceImportRunner(FinPriceDao finPriceDao, XiDataDao xiDataDao) {
 		super();
 		this.finPriceDao = finPriceDao;
 		this.xiDataDao = xiDataDao;
@@ -30,13 +41,20 @@ public class FinPriceImportRunner implements Runnable {
 
 	@Override
 	public void run() {
-		xiDataDao.findAllIds().forEach(t -> {
+		start = System.currentTimeMillis();
+		final Collection<Long> allIds = xiDataDao.findAllIds();
+
+		LOG.info(String.format("%s data sets staged", allIds.size()));
+
+		allIds.forEach(t -> {
 			try {
 				importData(t);
 			} catch (ParseException e) {
 				throw new BaseException(e);
 			}
 		});
+		end = System.currentTimeMillis();
+		LOG.info(String.format("%s data sets imported, took %s ms", imported, end - start));
 	}
 
 	private void importData(Long xiId) throws ParseException {
@@ -58,6 +76,7 @@ public class FinPriceImportRunner implements Runnable {
 
 		FinPrice build = new FinPrice.Builder().date(date).value(value).isin(isin).build();
 		finPriceDao.save(build);
+		imported++;
 
 	}
 }
