5fc52edafa8be8bb4f11974c1873cb843a98eec5
slipp <slipp@NB308.coinor.local>
2019-06-28 10:55:30 +0200

    refactoring

--- slag-finance/slag-finance-model/src/main/java/de/slag/finance/FinPriceDao.java
+++ slag-finance/slag-finance-model/src/main/java/de/slag/finance/FinPriceDao.java
@@ -11,22 +11,24 @@ import de.slag.common.base.BaseException;
 import de.slag.finance.model.FinPrice;
 
 public interface FinPriceDao extends Dao<FinPrice> {
-	
+
 	default Optional<FinPrice> loadPriceBy(String isin, Date date) {
 		Collection<Long> findAllIds = findAllIds();
-		final List<FinPrice> prices = findAllIds.stream()
+
+		final List<FinPrice> p1 = findAllIds.stream()
 				.map(id -> loadById(id))
 				.filter(v -> v.isPresent())
 				.map(v -> v.get())
-				.filter(v -> v instanceof FinPrice)
-				.map(v -> (FinPrice) v)
-				.filter(p -> p.getDate().equals(date))
+				.filter(p -> p.getDate().compareTo(date) == 0)
 				.filter(p -> p.getIsin().equals(isin))
 				.collect(Collectors.toList());
-		if (prices.size() > 1) {
+
+
+
+		if (p1.size() > 1) {
 			throw new BaseException("more than one!");
 		}
-		return prices.stream().findAny();
+		return p1.stream().findAny();
 	}
 
 }
