6b7642049ce1ab4b7f4ceda45735e644696b7d27
sebastian <sebastian@phobos.speedport.ip>
2019-06-28 08:19:13 +0200

    +data Store

---
+++ slag-finance/slag-finance-logic/src/main/java/de/slag/finance3/logic/FinDataStore.java
@@ -0,0 +1,15 @@
+package de.slag.finance3.logic;
+
+import java.time.LocalDate;
+import java.util.Optional;
+
+import de.slag.finance.data.model.Kpi;
+import de.slag.finance.model.AbstractFinDataPoint;
+
+public interface FinDataStore {
+
+	Optional<AbstractFinDataPoint> get(String isin, LocalDate date, Kpi kpi, Integer... params);
+
+	void put(AbstractFinDataPoint dp);
+
+}
