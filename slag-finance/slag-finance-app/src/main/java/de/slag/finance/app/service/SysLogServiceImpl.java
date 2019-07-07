335150f7d1b8a4a495f28680681095893caa77c8
slipp <slipp@NB308.coinor.local>
2019-07-07 12:01:51 +0200

    -default methods

--- slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/SysLogServiceImpl.java
+++ slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/SysLogServiceImpl.java
@@ -1,17 +1,11 @@
 package de.slag.finance.app.service;
 
-import java.util.Collection;
-import java.util.function.Predicate;
-import java.util.stream.Collectors;
-
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import de.slag.common.Dao;
 import de.slag.common.model.beans.SysLog;
-import de.slag.common.model.beans.SysLog.Severity;
-import de.slag.common.model.beans.SysLogEntry;
 import de.slag.common.model.dao.SysLogDao;
 import de.slag.common.model.service.SysLogService;
 
@@ -21,18 +15,6 @@ public class SysLogServiceImpl implements SysLogService {
 	@Resource
 	private SysLogDao sysLogDao;
 
-	@Override
-	public void log(Severity severity, String info) {
-		SysLog sysLog = new SysLog();
-		sysLog.setSeverity(severity);
-		sysLog.setInfo(info);
-		sysLogDao.save(sysLog);
-	}
-
-	@Override
-	public Collection<SysLogEntry> findBy(Predicate<SysLogEntry> filter) {
-		return sysLogDao.findAll().stream().filter(filter).collect(Collectors.toList());
-	}
 
 	@Override
 	public Dao<SysLog> getDao() {
