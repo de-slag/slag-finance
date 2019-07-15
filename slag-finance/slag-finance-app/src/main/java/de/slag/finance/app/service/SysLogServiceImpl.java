7a524d63d647d087be1f39b925e29b7fe9c30bf7
slipp <slipp@NB308.coinor.local>
2019-07-15 05:25:39 +0200

    SysLogService + log(Class, Severity, info)

--- slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/SysLogServiceImpl.java
+++ slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/SysLogServiceImpl.java
@@ -2,10 +2,12 @@ package de.slag.finance.app.service;
 
 import javax.annotation.Resource;
 
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
 
 import de.slag.common.Dao;
 import de.slag.common.model.beans.SysLog;
+import de.slag.common.model.beans.SysLog.Severity;
 import de.slag.common.model.dao.SysLogDao;
 import de.slag.common.model.service.SysLogService;
 
@@ -19,6 +21,11 @@ public class SysLogServiceImpl implements SysLogService {
 	@Override
 	public Dao<SysLog> getDao() {
 		return sysLogDao;
+	}
+	
+	@Override
+	public void log(Class<?> clazz, Severity severity, String info) {
+		SysLogService.super.log(clazz, severity, StringUtils.abbreviate(info, 4000));
 	}
 
 }
