28327f8ea051864b80e285371fd1317835f82e0c
slipp <slipp@NB308.coinor.local>
2019-07-15 05:24:22 +0200

    StockTitleDao: +findBy isin

--- slag-finance/slag-finance-model/src/main/java/de/slag/finance/StockTitleDao.java
+++ slag-finance/slag-finance-model/src/main/java/de/slag/finance/StockTitleDao.java
@@ -1,8 +1,16 @@
 package de.slag.finance;
 
+import java.util.Optional;
+
 import de.slag.common.Dao;
 import de.slag.finance.model.StockTitle;
 
 public interface StockTitleDao extends Dao<StockTitle> {
+
+	default Optional<StockTitle> findBy(String isin) {
+		return findAll().stream()
+					.filter(stockTitle -> stockTitle.getIsin().equals(isin))
+					.findAny();
+	}
 
 }
