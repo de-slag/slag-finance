f508c4b4999b467c1bfc80c8df00ea15705d7e44
slipp <slipp@NB308.coinor.local>
2019-07-31 18:41:54 +0200

    AbstractAdmServiceImpl

--- slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/FinAdmServiceImpl.java
+++ slag-finance/slag-finance-app/src/main/java/de/slag/finance/app/service/FinAdmServiceImpl.java
@@ -2,35 +2,28 @@ package de.slag.finance.app.service;
 
 import java.util.HashMap;
 import java.util.Map;
-import java.util.Optional;
 import java.util.Properties;
 import java.util.Set;
 
 import org.springframework.stereotype.Service;
 
-import de.slag.common.base.AdmService;
 import de.slag.common.base.SlagProperties;
+import de.slag.common.base.adm.AbstractAdmServiceImpl;
+import de.slag.common.base.adm.AdmService;
 
 @Service
-public class FinAdmServiceImpl  implements AdmService {
-	
-	private static final Map<String, String> MAP = new HashMap<>();
+public class FinAdmServiceImpl extends AbstractAdmServiceImpl implements AdmService {
 
-	static {
-		final Properties configProperties = SlagProperties.getConfigProperties();
-		final Set<Object> keySet = configProperties.keySet();
-		keySet.stream()
-			.filter(key -> key instanceof String)
-			.map(key -> (String) key)
-			.forEach(key -> MAP.put(key, configProperties.getProperty(key)));
-	}
+	public FinAdmServiceImpl() {
+		super(() -> {
+			final Properties configProperties = SlagProperties.getConfigProperties();
+			final Set<Object> keySet = configProperties.keySet();
+			final Map<String, String> map = new HashMap<>();
+			keySet.stream().filter(key -> key instanceof String).map(key -> (String) key)
+					.forEach(key -> map.put(key, configProperties.getProperty(key)));
 
-	@Override
-	public Optional<String> get(String key) {
-		if(!MAP.containsKey(key)) {
-			return Optional.empty();
-		}
-		return Optional.of(MAP.get(key));
+			return map;
+		});
 	}
 
 }
